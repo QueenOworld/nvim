61f5e7c5351df96960c8219017d452d691b42c2c
QueenOworld <queenoworld@disroot.org>
2025-05-06 06:14:16 -0400

    add some keybinds

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,10 @@
 -- plugins/telescope.lua:
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
--- or                              , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' },
-      config = function ()
-        require('telescope').setup{}
-      end
-    }
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    -- or                              , branch = '0.1.x',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+        require('telescope').setup {}
+    end
+}
